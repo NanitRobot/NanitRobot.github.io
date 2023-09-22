1413e50313d370b12a1a487e40cc2a0a04a8a622
Sam4uk <sto.ukr.net@gmail.com>
2023-09-22 17:06:56 +0300

    п'ятниця, 22 вересня 2023 17:06:56 +0300

--- runme.sh
+++
@@ -1,9 +0,0 @@
-# Оновлюємо субмодулі
-git submodule update --init --recursive
-# Копіюю пін варіант
-cp MegaCore/avr/variants/100-pin-arduino-mega/pins_arduino.h Nanit/variants/Nanit/pins_arduino.h 
-# копіюю ядро    
-cp -ruf MegaCore/avr/cores/MCUdude_corefiles/* Nanit/cores/MegaCore/  
-# все пакуємо до архіву
-zip -qq -r Nanit_Boards.zip Nanit
-git add Nanit_Boards.zip
